f5b162041cd2fb3670381c3ff967e26751cdc54e
Nate Roberts <robernat@msudenver.edu>
2019-01-28 14:10:24 -0700

    Added myself to the program output and test code; also added a line specifying that the listed names are Team 5.

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -1,8 +1,10 @@
 def HelloWorld():
     print("Hello world")
+    print("Team 5 is:")
     print("Brian Pedersen")
     print("Jake Schnorr")
     print("Megan Stucky")
     print("James Thomas")
+    print("Nate Roberts")
 if '__main__' == __name__:
     HelloWorld()
