05544106ae1fd6e2db448686da7a005cd02fe01c
LSmith-Zenoscave <lsmith@zenoscave.com>
2019-02-04 17:31:43 -0700

    pycharm unitest patch
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -1,31 +1,19 @@
 import unittest
-import sys
 import jvpm.HelloWorld
-from unittest.mock import Mock, call
+from unittest.mock import patch, call
 
 class TestHelloWorld(unittest.TestCase):
-    def test_HelloWorld(self):
-        sys.stdout = unittest.mock.Mock()
+    @patch('builtins.print')
+    def test_HelloWorld(self, mock_patch):
         jvpm.HelloWorld.HelloWorld()
-        sys.stdout.assert_has_calls(
-            [
-                call.write('Hello world'),
-                call.write('\n'),
-                call.write('Team 5 is:'),
-                call.write('\n'),
-                call.write('Brian Pedersen'),
-                call.write('\n'),
-                call.write('Jake Schnorr'),
-                call.write('\n'),
-                call.write('Megan Stucky'),
-                call.write('\n'),
-                call.write('James Thomas'),
-                call.write('\n'),
-                call.write('Nate Roberts'),
-                call.write('\n'),
-                call.write('John Shapiro'),
-                call.write('\n'),
-		call.write('Kelly Trujillo'),
-                call.write('\n'),
-            ]
-        )
+        self.assertEqual(mock_patch.mock_calls, [
+            call.write('Hello world'),
+            call.write('Team 5 is:'),
+            call.write('Brian Pedersen'),
+            call.write('Jake Schnorr'),
+            call.write('Megan Stucky'),
+            call.write('James Thomas'),
+            call.write('Nate Roberts'),
+            call.write('John Shapiro'),
+            call.write('Kelly Trujillo'),
+        ])
