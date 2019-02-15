9a36248d3318bcf8768630f01cf63f76c4b19858
johnhshapiro <jshapir4@msudenver.edu>
2019-02-15 15:56:55 -0700

    Fixed file path to not try to use my computers directory

--- jvpm/test/test_class_file.py
+++ jvpm/test/test_class_file.py
@@ -1,4 +1,5 @@
 """this is a test"""
+import os
 import unittest
 from unittest.mock import mock_open, patch
 from jvpm.class_file import ClassFile
@@ -8,7 +9,7 @@ class TestClassFile(unittest.TestCase):
         """set up the test"""
         mock_object = mock_open(read_data=b'\xCA\xFE\xBA\xBE\x00\x00\x004\x00\x0f')
         with patch(__name__ + '.open', mock_object):
-            self.class_file = ClassFile('C:\\Users\\John Shapiro\\3250-spring-2019-team-5\\jvpm\\Test.class')
+            self.class_file = ClassFile('jvpm/Test.class')
 
     def test_magic(self):
         """tests the get_magic method"""
