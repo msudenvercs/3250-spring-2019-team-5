c426a35d057a86b382e500f07e3cb736bdb5834b
James Thomas <jjt91@msn.com>
2019-02-23 21:56:52 -0700

    implemented iadd and isub operations. Added jvm_stack.py to this branch since it is needed in iaadded and isub. Fixed all pylint violations.

--- jvpm/op_codes.py
+++ jvpm/op_codes.py
@@ -1,19 +1,27 @@
 """this file contains the OpCodes class"""
+import numpy #to get the java-like behavior for arithmetic
+
+from jvpm.jvm_stack import JvmStack
+
+#shuts off the overflow warnings from numpy
+numpy.seterr(all="ignore")
 class OpCodes():
     """This class is used for the interpretation of opcodes"""
     def __init__(self):
         """this is the constructor"""
         with open('jvpm/Test.class', 'rb') as binary_file:
             self.data = bytes(binary_file.read())
-        self.table = {0x2a: aload_0, 
+        self.table = {0x2a: aload_0,
                       0xb1: ret,
                       0x04: iconst_1,
                       0x3c: istore_1,
                       0x84: iinc,
                       0xb7: invokespecial,
+                      0x60: iadd,
+                      0x64: isub,
                       0x00: not_implemented}
         self.byte_count = 0
-
+        self.stack = JvmStack()
     def parse_codes(self, op_start):
         """this method searches the binary for only the opcodes we know are in it"""
         self.byte_count = op_start
@@ -28,38 +36,54 @@ class OpCodes():
         return self.table[value](self)
 
 def not_implemented(self):
+    """this is a dummy function"""
     self.byte_count += 1
     return 'not implemented'
 
 def aload_0(self):
     """this is a dummy method"""
     print('aload_0')
-    self.byte_count +=1
+    self.byte_count += 1
 
 def ret(self):
+    """this function will eventually implement the ret opcode"""
     self.byte_count += 1
     print('return')
 
 def iconst_1(self):
+    """this function implements the iconst_1 opcode"""
     self.byte_count += 1
     print('iconst_1')
 
 def istore_1(self):
+    """this function implements the istore_1 opcode"""
     self.byte_count += 1
     print('istore_1')
 
 def iinc(self):
+    """this function implements the iinc opcode"""
     self.byte_count += 1
     print('iinc')
 
 def invokespecial(self):
+    """This function implements the invokespecial opcode"""
     byte_1 = self.data[self.byte_count + 1]
     byte_2 = self.data[self.byte_count + 2]
     self.byte_count += 3
-    byte_1 + byte_2
     print('invokespecial')
-    
-
+    return byte_1+byte_2
+def iadd(self):
+    """implements the iadd opcode"""
+    val2 = self.stack.pop_op()
+    val1 = self.stack.pop_op()
+    self.stack.push_op(val1+val2)
+    self.byte_count += 1
+def isub(self):
+    """implements the isub opcode"""
+    val2 = self.stack.pop_op()
+    val1 = self.stack.pop_op()
+    self.stack.push_op(val1-val2)
+    self.byte_count += 1
 if __name__ == '__main__':
-    op = OpCodes()
-    op.parse_codes(183)
+    OP = OpCodes()
+    OP.parse_codes(183)
--- jvpm/test/test_op_codes.py
+++ jvpm/test/test_op_codes.py
@@ -1,7 +1,12 @@
 """this is a test for op_codes"""
 import unittest
 from unittest.mock import patch, call
+import numpy
+from jvpm.op_codes import iadd, isub
 from jvpm.op_codes import OpCodes
+numpy.warnings.filterwarnings("ignore")
+
+
 class TestOpCodes(unittest.TestCase):
     """this class tests the op_codes class"""
     @patch('builtins.print')
@@ -10,25 +15,41 @@ class TestOpCodes(unittest.TestCase):
         op_code = OpCodes()
         op_code.parse_codes(0)
         self.assertEqual(mock_patch.mock_calls,
-[call('istore_1'),
- call('iconst_1'),
- call('iconst_1'),
- call('iconst_1'),
- call('iconst_1'),
- call('iconst_1'),
- call('aload_0'),
- call('invokespecial'),
- call('return'),
- call('iconst_1'),
- call('istore_1'),
- call('iinc'),
- call('return')])
+                         [call('istore_1'),
+                          call('iconst_1'),
+                          call('iconst_1'),
+                          call('iconst_1'),
+                          call('iconst_1'),
+                          call('iconst_1'),
+                          call('aload_0'),
+                          call('invokespecial'),
+                          call('return'),
+                          call('iconst_1'),
+                          call('istore_1'),
+                          call('iinc'),
+                          call('return')])
+
     def test_not_implmented(self):
         """this method tests the OpCodes class"""
         self.assertEqual(OpCodes().interpret(0), 'not implemented')
 
     def test_mph1(self):
         """a"""
-        self.assertEqual(1+1,2)
+        self.assertEqual(1 + 1, 2)
         with self.assertRaises(KeyError):
             OpCodes().interpret(1)
+
+    def test_add_subtract(self):
+        """tests the iadd and isub opcodes"""
+        op_code = OpCodes()
+        op_code.stack.push_op(numpy.int32(2000000000))
+        op_code.stack.push_op(numpy.int32(1000000000))
+
+        iadd(op_code)
+        self.assertEqual(op_code.stack.peek(), -1294967296)
+        self.assertEqual(op_code.byte_count, 1)
+        op_code.stack.push_op(numpy.int32(10))
+        op_code.stack.push_op(numpy.int32(3))
+        isub(op_code)
+        self.assertEqual(op_code.stack.peek(), 7)
+        self.assertEqual(op_code.byte_count, 2)
