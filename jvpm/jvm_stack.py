26ced6d23c24d0761760e77131627c12802817cb
James Thomas <jjt91@msn.com>
2019-04-07 09:18:43 -0600

    refactored op_codes to remove repeated checks for empty stack; moved such checking into the pop_op method in the jvm_stack class

--- jvpm/jvm_stack.py
+++ jvpm/jvm_stack.py
@@ -9,10 +9,16 @@ class JvmStack:
         self.stack.append(obj)
     def pop_op(self):
         """Pops one object off of the stack and then returns it"""
-        return self.stack.pop()
+        if self.stack:
+            return self.stack.pop()
+        else:
+            raise EmptyStackError("Can't pop from an empty stack")
     def peek(self):
         """returns the top element of the stack without popping it.
 This doesn't seem useful now,
 but it is one of the standard stack operations that might be useful later
 """
         return self.stack[-1]
+class EmptyStackError(Exception):
+    """error raised when there is an empty stack"""
+    pass #dummy statement that does nothing; fixes syntax error.
