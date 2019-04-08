c0a69f7c44dbfd11f43958d5565a0bf1bdaa3ad2
johnhshapiro <jshapir4@msudenver.edu>
2019-04-08 12:58:12 -0600

    temporarily deletion of AddTwo to test build

--- jvpm/AddTwo.java
+++
@@ -1,9 +0,0 @@
-public class AddTwo {
-    public static void main (String[] args) throws java.io.IOException {
-        java.util.Scanner reader = new java.util.Scanner(System.in);
-        int a = reader.nextInt();
-        int b = reader.nextInt();
-        System.out.println (a+b);
-        reader.close();
-    }
-}
